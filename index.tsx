56c8d1d94a9365cd30263e5c29a989575a1e6754
jnitapebii-source <jnitapebii@gmail.com>
2026-02-12 10:13:26 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App';;
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <div style={{ padding: 40, fontSize: 24 }}>
+    LinkFlow está funcionando!  🚀
+  </div>
+);
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
